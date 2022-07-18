e28ef8f9169194eb4afea3fda65d1110e2bf13c0
haril <songkg7@gmail.com>
2022-07-18 20:46:28 +0900

    refactor: func 분리

--- main.go
+++ main.go
@@ -6,27 +6,36 @@ import (
 	"net/http"
 )
 
-func main() {
-	router := gin.Default()
-	router.GET("/ping", func(context *gin.Context) {
+func ping() func(context *gin.Context) {
+	return func(context *gin.Context) {
 		context.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
-	})
+	}
+}
 
-	router.GET("/person", func(context *gin.Context) {
+func getPerson() func(context *gin.Context) {
+	return func(context *gin.Context) {
 		person := Person{Name: "haril", Age: 28}
 		context.JSON(http.StatusOK, person)
-	})
+	}
+}
 
-	router.POST("/person/create", func(context *gin.Context) {
+func create() func(context *gin.Context) {
+	return func(context *gin.Context) {
 		var req Person
 		if err := context.BindJSON(&req); err != nil {
 			fmt.Println(err.Error())
 			return
 		}
 		fmt.Println(req.Name, req.Age)
-	})
+	}
+}
 
+func main() {
+	router := gin.Default()
+	router.GET("/ping", ping())
+	router.GET("/person", getPerson())
+	router.POST("/person/create", create())
 	router.Run()
 }
