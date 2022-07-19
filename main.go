1082d48a1d5583632a4336805413c325438992ec
haril <songkg7@gmail.com>
2022-07-19 15:54:45 +0900

    db setting

--- main.go
+++ main.go
@@ -4,11 +4,26 @@ import (
 	"database/sql"
 	"fmt"
 	"github.com/cenkalti/backoff/v4"
+	"github.com/cockroachdb/cockroach-go/v2/crdb"
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
 	"os"
 )
+
+func main() {
+	router := gin.Default()
+	db, err := initStore()
+	if err != nil {
+		log.Fatalf("failed to initialise the store: %s", err)
+	}
+	defer db.Close()
+
+	router.GET("/ping", ping())
+	router.GET("/person", getPerson())
+	router.POST("/person", create(db))
+	router.Run()
+}
 
 func ping() func(context *gin.Context) {
 	return func(context *gin.Context) {
@@ -25,29 +40,31 @@ func getPerson() func(context *gin.Context) {
 	}
 }
 
-func create() func(context *gin.Context) {
+type Message struct {
+	Value string `json:"value"`
+}
+
+func create(db *sql.DB) func(context *gin.Context) {
 	return func(context *gin.Context) {
+		m := &Message{}
 		var req Person
 		if err := context.BindJSON(&req); err != nil {
 			fmt.Println(err.Error())
 			return
 		}
+		crdb.ExecuteTx(context, db, nil, func(tx *sql.Tx) error {
+			_, err := tx.Exec(
+				"INSERT INTO message (value) VALUES ($1) ON CONFLICT (value) DO UPDATE SET value = excluded.value",
+				m.Value,
+			)
+			if err != nil {
+				//return context.JSON(http.StatusInternalServerError, err)
+				return context.Error(err)
+			}
+			return nil
+		})
 		context.JSON(http.StatusCreated, req)
 	}
-}
-
-func main() {
-	router := gin.Default()
-	db, err := initStore()
-	if err != nil {
-		log.Fatalf("failed to initialise the store: %s", err)
-	}
-	defer db.Close()
-
-	router.GET("/ping", ping())
-	router.GET("/person", getPerson())
-	router.POST("/person/create", create())
-	router.Run()
 }
 
 func initStore() (*sql.DB, error) {
