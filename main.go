ce1d61d1831dcaef51faf6e9209a65830878ef2d
haril <songkg7@gmail.com>
2022-07-23 13:57:43 +0900

    gorm 과 REST api 의 결합

--- main.go
+++ main.go
@@ -1,21 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"net/http"
+	"os"
 )
 
 type Product struct {
 	gorm.Model
-	Code  string
-	Price uint
+	Code  string `json:"code"`
+	Price uint   `json:"price"`
 }
 
 func main() {
 	router := gin.Default()
-	db, err := gorm.Open(postgres.Open("test.db"), &gorm.Config{})
+	pgConnString := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=disable",
+		os.Getenv("PGHOST"),
+		os.Getenv("PGPORT"),
+		os.Getenv("PGDATABASE"),
+		os.Getenv("PGUSER"),
+		os.Getenv("PGPASSWORD"),
+	)
+	db, err := gorm.Open(postgres.Open(pgConnString), &gorm.Config{})
 	if err != nil {
 		panic("DB 연결에 실패하였습니다.")
 	}
@@ -23,26 +32,32 @@ func main() {
 	// 테이블 자동생성
 	db.AutoMigrate(&Product{})
 
-	// 생성
-	db.Create(&Product{
-		Code:  "D42",
-		Price: 100,
-	})
-
-	// 읽기
 	var product Product
-	db.First(&product, 1)                 // primary key 기준으로 product 찾기
-	db.First(&product, "code = ?", "D42") // code 가 D42 인 product 찾기
 
-	// 수정 - product 의 price 를 200 으로
-	db.Model(&product).Update("Price", 200)
-
-	// 수정 - 여러개의 필드를 수정하기
-	db.Model(&product).Updates(Product{Price: 200, Code: "F42"})
-	db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
-
-	// 삭제 - product 삭제하기
-	db.Delete("&product", 1)
+	router.GET("/:id", func(context *gin.Context) {
+		// 읽기
+		db.First(&product, 1)                 // primary key 기준으로 product 찾기
+		db.First(&product, "code = ?", "D42") // code 가 D42 인 product 찾기
+	})
+	router.POST("/create", func(context *gin.Context) {
+		// 생성
+		result := db.Create(&Product{
+			Code:  "D42",
+			Price: 100,
+		})
+		context.JSON(http.StatusCreated, result.RowsAffected)
+	})
+	router.PATCH("/:id", func(context *gin.Context) {
+		// 수정 - product 의 price 를 200 으로
+		db.Model(&product).Update("Price", 200)
+		// 수정 - 여러개의 필드를 수정하기
+		//db.Model(&product).Updates(Product{Price: 200, Code: "F42"})
+		//db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
+	})
+	router.DELETE("/delete/:id", func(context *gin.Context) {
+		// 삭제 - product 삭제하기
+		db.Delete("&product", 1)
+	})
 
 	router.GET("/ping", ping())
 	router.Run()
